0bd08b02a830c86dd8279a112e0a53e8c6af08fe
lucagad <90861515+lucagad@users.noreply.github.com>
2022-04-07 14:35:14 +0200

    {JS} Added: code to show / hide the hamburger menu

---
+++ main.js
@@ -0,0 +1,23 @@
+//copio l'elemento <div class="header-right"> all'interno della variabile btnHamburger
+
+const btnHamburger = document.querySelector("div.header-right");
+
+console.log (btnHamburger);
+
+//copio l'elemento <div class="hamburger-menu">  all'interno della variabile hamburgerMenu
+const hamburgerMenu = document.querySelector("div.hamburger-menu");
+
+//copio l'elemento <i class="fas fa-times"> all'interno della variabile btnCross
+const btnCross = document.querySelector("i.fas.fa-times");
+console.log (btnCross);
+
+btnHamburger.addEventListener('click', function(){
+  // qui vengono eseguiti i comandi al click del hamburger 
+  hamburgerMenu.classList.add('active');
+});
+
+
+btnCross.addEventListener('click', function(){
+  // qui vengono eseguiti i comandi al click della X
+  hamburgerMenu.classList.remove('active');
+});
